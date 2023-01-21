be5655eb7d33a45db786e63bc3c20d3f0c11a8f2
shrikantja <85014649+shrikantja@users.noreply.github.com>
2023-01-21 13:19:04 +0530

    Update start_httpd.sh
    super user added

--- start_httpd.sh
+++ start_httpd.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sudo su
-systemctl start httpd
-chmod 777 /var/www/html/index.html
+
 
 # Here is where you'd want to start your http daemon. For example:
-#service httpd start
+service httpd start
 #exit $?
 
 # In this case, since it's just a placeholder, we don't need to do anything.
