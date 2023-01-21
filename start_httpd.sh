b7af4012b1e9df32f227a6215b85fd0dd847a916
shrikantja <85014649+shrikantja@users.noreply.github.com>
2023-01-21 14:14:01 +0530

    Update start_httpd.sh

--- start_httpd.sh
+++ start_httpd.sh
@@ -4,7 +4,7 @@ sudo su
 
 # Here is where you'd want to start your http daemon. For example:
 service httpd start
-#exit $?
+exit $?
 
 # In this case, since it's just a placeholder, we don't need to do anything.
 # exit 0
